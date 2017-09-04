ec56bcec7a65debb9e7d673d4da4752d36533157
katenorquay <katenorquay@live.com>
2017-09-04 14:47:03 +1200

    moved field specific logic into addressGroup

--- src/_formField.js
+++ src/_formField.js
@@ -5,40 +5,23 @@
  *  - an associated mapping to the API / Metadata
  *  - a method for updating the form field value
  */
-(function(w){
 
-  function FormField(selector, mappingId){
-    var f = this;
-    f.mappingId = mappingId;
-    f.selector = selector;
-    f.element = function(){
-      return document.querySelector(f.selector);
-    };
+ (function(w){
 
-    f.addressLine1 = function() {
-      var checkoutTypes = ['shipping', 'billing'];
-      for (var i = 0; i < checkoutTypes.length; i++) {
-        var element = document.getElementById('checkout_' + checkoutTypes[i] + '_address_address1');
-        if (element) return element;
-      }
-    };
+   function FormField(selector, mappingId){
+     var f = this;
+     f.mappingId = mappingId;
+     f.selector = selector;
+     f.element = function(){
+       return document.querySelector(f.selector);
+     };
+     f.setValue = function(value) {
+       if (value === undefined || value === null) value = '';
+       if (f.element() === null) return;
+       f.element().value = value;
+     };
 
-    f.setValue = function(value) {
-      if (value === undefined || value === null) {
-        value = '';
-      }
-
-      if (value !== '' && f.element() === null) {
-        var formattedValue = ', ' + value;
-        f.addressLine1().value += formattedValue;
-      } else if (f.element() === null) {
-        return;
-      } else {
-        f.element().value = value;
-      }
-    };
-
-    return f;
-  }
-  w.AF ? w.AF.FormField = FormField : w.AF = {FormField: FormField};
-})(window);
+     return f;
+   }
+   w.AF ? w.AF.FormField = FormField : w.AF = {FormField: FormField};
+ })(window);
