8885e66e4fa656b0fed6dd0617508c5fcc6451c3
katenorquay <katenorquay@live.com>
2019-04-12 12:23:55 +1200

    added npm package

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['es2015']
+            presets: ['env']
           }
         }
       }
