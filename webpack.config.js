b3758b4fb994cf5bf89acbe4c48faef62f07154a
katenorquay <katenorquay@live.com>
2019-04-29 14:20:51 +1200

    updated packages

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['@babel/preset-env']
           }
         }
       }
