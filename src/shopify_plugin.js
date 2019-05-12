7607e61d9d5a6dab7fe0dca95dea5ac783749f38
katenorquay <katenorquay@live.com>
2019-05-13 09:29:18 +1200

    added npm package

--- src/shopify_plugin.js
+++ src/shopify_plugin.js
@@ -1,5 +1,5 @@
 import ConfigManager from './config_manager'
-import { PageManager, MutationManager } from './addressfinder-webpage-tools'
+import { PageManager, MutationManager } from '@addressfinder/addressfinder-webpage-tools'
 
 (function(d, w) {
 
